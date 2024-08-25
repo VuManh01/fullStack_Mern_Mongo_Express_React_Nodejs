f39bfa0ffe3cda740945d69e07c3fbdca07e5d5c
VuManh01 <vutrongmanhmk@gmail.com>
2024-08-25 13:13:22 +0700

    1:13_25/8/2024

--- server/models/User.js
+++ server/models/User.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema
 const UserSchema = new Schema({
     username: {
         type: String,
-        required :true,
+        required: true,
         unique: true
     },
     password: {
