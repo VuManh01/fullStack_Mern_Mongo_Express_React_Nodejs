3cce533a36e0ce4a118e04d83bc37d4141cbb837
VuManh01 <vutrongmanhmk@gmail.com>
2024-08-26 23:31:01 +0700

    11:30_26/8/2023

--- server/index.js
+++ server/index.js
@@ -1,29 +1,29 @@
-require('dotenv').config()
-const express = require('express')
-const mongoose = require('mongoose')
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
 
-const authRouter = require('./routes/auth')
+const authRouter = require("./routes/auth");
+const postRouter = require("./routes/post");
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(
-            `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@mrenproject.rmgfmtg.mongodb.net`,
-        )
-        console.log('MongoDB connected');
-    } catch (error) {
-        console.log(error.message);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@mrenproject.rmgfmtg.mongodb.net`
+    );
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
 };
 
-connectDB()
+connectDB();
 
 const app = express();
 app.use(express.json());
 
-app.use('/api/auth', authRouter);
-
-
+app.use("/api/auth", authRouter);
+app.use("/api/posts", postRouter);
 
 const PORT = 5000;
 
